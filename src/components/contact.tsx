0092d105ea2c575a9cdf31b424f3731d7c948bf0
Joshua Palamuttam <joshpal97@gmail.com>
2020-05-02 16:42:55 -0700

    built up the project and contact page and added some pictures

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,13 +1,52 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
+import ContactPicture from "../images/forest_selfie.jpg";
 class Contact extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Contact Page</h1>
+  render() {
+    return (
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Joshua Palamuttam</h2>
+            <img
+              src={ContactPicture}
+              alt="contact-picture"
+              style={{ height: "250px" }}
+            />
+            <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
+              Tell me about yourself
+            </p>
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
+
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: "30px", fontFamily: "Anton" }}
+                  >
+                    <i className="fa fa-phone-square" aria-hidden="true" />
+                    (408) 841-6194
+                  </ListItemContent>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: "30px", fontFamily: "Anton" }}
+                  >
+                    <i className="fa fa-envelope" aria-hidden="true" />
+                    joshpal97@gmail.com
+                  </ListItemContent>
+                </ListItem>
+              </List>
             </div>
-        )
-    }
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default Contact;
